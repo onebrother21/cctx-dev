eb19988c2faa63cbcf1de3b62f6026915be20885
onebrother21 <service.onebrother@gmail.com>
2022-03-13 23:20:36 -0500

    rebuilding with major changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const appStatic = path.join(__dirname,'/dist/oba-apps');
+const appStatic = path.join(__dirname,'/dist/cctx-apps');
 app.use(express.static(appStatic));
 app.get('/*',(req,res) => res.sendFile(appStatic + '/index.html'));
 app.listen(process.env.PORT || 8080);
