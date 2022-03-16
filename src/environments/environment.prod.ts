fb7c089e5cf19f6f555642a0baff146350534d94
onebrother21 <service.onebrother@gmail.com>
2022-03-16 16:27:45 -0500

    rebuilding with minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,12 @@ export const environment = {
   production: true,
   prod:true,
   debug:false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDrVriUAjBONjx06y3NnGJc7tTyzN-5kUw",
+    authDomain: "cctx-apps.firebaseapp.com",
+    projectId: "cctx-apps",
+    storageBucket: "cctx-apps.appspot.com",
+    messagingSenderId: "1098794307295",
+    appId: "1:1098794307295:web:6bb0f39b1762f70193e36a"
+  },
 };
